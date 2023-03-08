a66cb36609d07eda284395e995f71f511ba647c9
Fathan Mubarok <falonez01@gmail.com>
2023-03-08 10:39:49 +0700

    init

---
+++ components/Welcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <main class="mx-auto p-6 flex items-center justify-center h-screen">
+      <div class="w-[50%] text-blue-500">Test</div>
+    </main>
+  </div>
+</template>
